fccea6d389c23b7d6518b346252522b1a042c00a
Kostja P <git@kostja.me>
2025-02-01 08:57:33 +0100

    new init

--- init.sh
+++ init.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
+
+
+open_url() {
+    local url=$1
+
+    case "$(uname -s)" in
+        Linux*)
+            if grep -qi microsoft /proc/version; then
+                # WSL
+                cmd.exe /c start "$url" >/dev/null 2>&1
+            else
+                # Regular Linux
+                xdg-open "$url" >/dev/null 2>&1
+            fi
+            ;;
+        Darwin*)
+            # macOS
+            open "$url" >/dev/null 2>&1
+            ;;
+        *)
+            echo "Unsupported operating system"
+            exit 1
+            ;;
+    esac
+}
+
 git init -b master
 git add .
 git commit -m "Initial commit"
@@ -12,5 +38,5 @@ docker compose run --rm web python manage.py migrate
 docker compose run --rm web python manage.py makesuperuser
 
 docker compose up -d
-open https://127.0.0.1:9000
+open_url https://127.0.0.1:9000
 echo "Open your browser at http://127.0.0.1:9000"

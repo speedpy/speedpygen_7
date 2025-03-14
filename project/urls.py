ea9077a8e692b5c01a27e809da3fc551060a9b3c
Kostja P <git@kostja.me>
2025-03-14 23:55:33 +0100

    Added empty mainapp/urls.py

--- project/urls.py
+++ project/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
+    path("", include("mainapp.urls")),
 ]
